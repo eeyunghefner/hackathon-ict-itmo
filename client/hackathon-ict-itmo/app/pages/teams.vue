523cdbe3a5c713542c45036628cb1f55bcdde3fd
NorthPole0499 <alekseev-timof@mail.ru>
2026-03-13 17:34:53 +0300

    Added teams page

---
+++ client/hackathon-ict-itmo/app/pages/teams.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Navbar />
+
+    <h1>Поиск команд</h1>
+
+    <input v-model="search" placeholder="Поиск команды" />
+
+    <TeamTable :teams="filteredTeams" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue"
+import TeamTable from "~/components/TeamTable.vue"
+import { useTeamStore } from "~/stores/teamStore"
+import { ref, computed } from "vue"
+
+const teamStore = useTeamStore()
+
+const search = ref("")
+
+const filteredTeams = computed(() =>
+  teamStore.searchTeams(search.value)
+)
+</script>
