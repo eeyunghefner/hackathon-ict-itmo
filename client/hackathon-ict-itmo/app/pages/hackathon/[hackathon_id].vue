bf5703903bc37511d9639f45882ffcf202aea47b
NorthPole0499 <alekseev-timof@mail.ru>
2026-03-13 17:37:35 +0300

    Added hackathon page

---
+++ client/hackathon-ict-itmo/app/pages/hackathon/[hackathon_id].vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Navbar />
+
+    <h1>{{ hackathon?.name }}</h1>
+
+    <ScheduleTable :events="events" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router"
+import Navbar from "~/components/Navbar.vue"
+import ScheduleTable from "~/components/ScheduleTable.vue"
+import { useHackathonStore } from "~/stores/hackathonStore"
+
+const route = useRoute()
+
+const hackathonStore = useHackathonStore()
+
+const id = route.params.hackathon_id as string
+
+const hackathon = hackathonStore.getHackathonById(id)
+
+const events = hackathonStore.getScheduleByHackathonId(id)
+</script>
