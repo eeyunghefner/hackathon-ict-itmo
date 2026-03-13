6eb7bf0901725a7a2e740313ac5caf83b5437630
NorthPole0499 <alekseev-timof@mail.ru>
2026-03-13 17:29:16 +0300

    Added user profile page

---
+++ client/hackathon-ict-itmo/app/pages/me.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <Navbar />
+
+    <h1>Личный кабинет</h1>
+
+    <div>
+      <label>ФИО</label>
+      <input v-model="user.fullName" />
+    </div>
+
+    <div>
+      <label>Образование</label>
+      <input v-model="user.education" />
+    </div>
+
+    <div>
+      <label>Навыки</label>
+      <input v-model="user.skills" />
+    </div>
+
+    <div>
+      <label>Описание</label>
+      <textarea v-model="user.description"></textarea>
+    </div>
+
+    <button @click="save">Сохранить</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from "~/components/Navbar.vue"
+import { useUserStore } from "~/stores/userStore"
+
+const userStore = useUserStore()
+
+const user = userStore.user
+
+function save() {
+  userStore.updateUser(user)
+}
+</script>
