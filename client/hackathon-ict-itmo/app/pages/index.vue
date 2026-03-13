ce33069d292a6ba678dc5cdbe4ef0654d1a22282
NorthPole0499 <alekseev-timof@mail.ru>
2026-03-13 17:25:29 +0300

    Added main user page with store

--- client/hackathon-ict-itmo/app/pages/index.vue
+++ client/hackathon-ict-itmo/app/pages/index.vue
@@ -1,6 +1,17 @@
 <template>
-    Hello
+  <div>
+    <Navbar />
+
+    <h1>Мои хакатоны</h1>
+
+    <HackathonTable :hackathons="hackathonStore.hackathons" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import Navbar from "~/components/Navbar.vue"
+import HackathonTable from "~/components/HackathonTable.vue"
+import { useHackathonStore } from "~/stores/hackathonStore"
+
+const hackathonStore = useHackathonStore()
 </script>
